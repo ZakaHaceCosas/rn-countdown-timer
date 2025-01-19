3b19e4e71b12f252fa5d3cbc93ce230d4ad54a1c
Zaka <zakahacecosas@protonmail.com>
2025-01-19 08:24:48 +0100

    chore(build): Tweak build file

--- scripts/build.js
+++ scripts/build.js
@@ -2,17 +2,13 @@ const fs = require('fs')
 const esbuild = require('esbuild')
 const pkg = require('../package.json')
 
-const commonProps = {
-  entryPoints: [pkg.source],
-  bundle: true,
-  minify: true,
-  external: ['react', 'react-native', 'react-native-svg'],
-}
-
 esbuild
   .build({
-    ...commonProps,
     outfile: pkg.main,
+    entryPoints: [pkg.source],
+    bundle: true,
+    minify: true,
+    external: ['react', 'react-native', 'react-native-svg'],
     format: 'esm',
   })
   .then(() => {
